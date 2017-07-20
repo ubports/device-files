aadbda9f0296c69d2ebe30c9be843460a5b4e057
Marius Gripsgard <mariogrip@ubuntu.com>
2017-07-20 13:53:16 +0200

    Revert "Add indicator torch to rootfs"

--- common/system/opt/indicator-torch.nfsprodriver/indicator/uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-rm /home/phablet/.config/upstart/nfsprodriver-indicator-torch.conf
-rm /home/phablet/.local/share/unity/indicators/com.nfsprodriver.indicator.torch
-
-echo "indicator-torch uninstalled"
